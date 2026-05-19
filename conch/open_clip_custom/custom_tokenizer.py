ac7b15760a3bcad8c7a9168b528490a2f2daa49d
Sean Browning <sbrowning@cdc.gov>
2026-05-19 19:04:57 +0000

    Small fixes to get basics_usage running with current package versions

--- conch/open_clip_custom/custom_tokenizer.py
+++ conch/open_clip_custom/custom_tokenizer.py
@@ -1,8 +1,10 @@
 import os
 
 from pathlib import Path
+import torch
 import torch.nn.functional as F
 from transformers import PreTrainedTokenizerFast
+from transformers.tokenization_utils_base import TruncationStrategy
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -16,15 +18,16 @@ def get_tokenizer():
                                         pad_token="<pad>")
     return tokenizer
 
-def tokenize(tokenizer, texts):
+def tokenize(tokenizer: PreTrainedTokenizerFast, texts: list[str]) -> torch.Tensor:
     # model context length is 128, but last token is reserved for <cls>
     # so we use 127 and insert <pad> at the end as a temporary placeholder
-    tokens = tokenizer.batch_encode_plus(texts, 
+    tokens = tokenizer._encode_plus(texts, 
                                         max_length = 127,
                                         add_special_tokens=True, 
                                         return_token_type_ids=False,
-                                        truncation = True,
-                                        padding = 'max_length',
-                                        return_tensors = 'pt')
+                                        truncation_strategy = TruncationStrategy.LONGEST_FIRST,
+                                        padding_strategy = "max_length",
+                                        return_tensors = "pt")
+
     tokens = F.pad(tokens['input_ids'], (0, 1), value=tokenizer.pad_token_id)
     return tokens
